cde7f1e2406d44582d792156f6930ea61c53d475
leenton <cakeunit01@gmail.com>
2022-12-10 17:08:09 +0000

    rest

---
+++ endpoints/test.ps1
@@ -0,0 +1,3 @@
+function test($I){
+    write-host $I 
+}
--- src/PSRestServer.py
+++ src/PSRestServer.py
@@ -1,4 +1,3 @@
-from http.server import BaseHTTPRequestHandler, HTTPServer
 from pathlib import Path
 import json, configparser, ssl
 
@@ -8,43 +7,6 @@ hello = {
     "department": "HR"
 }
 
-class AppServer(BaseHTTPRequestHandler):
-    def do_GET(self):
-        print(self.headers)
-        self.send_response(200)
-        self.send_header("Content-type", "application/json")
-        self.end_headers()
-        self.wfile.write(bytes(json.dumps(hello), "utf-8"))
-    
-    def do_HEAD(self):
-        self.send_response(200)
-        self.send_header("Content-type", "application/json")
-        self.end_headers()
-        self.wfile.write(bytes(json.dumps(hello), "utf-8"))
-    
-    def do_POST(self):
-        self.send_response(200)
-        self.send_header("Content-type", "application/json")
-        self.end_headers()
-        self.wfile.write(bytes(json.dumps(hello), "utf-8"))
-
-    def do_PUT(self):
-        self.send_response(200)
-        self.send_header("Content-type", "application/json")
-        self.end_headers()
-        self.wfile.write(bytes(json.dumps(hello), "utf-8"))
-    
-    def do_DELETE(self):
-        self.send_response(200)
-        self.send_header("Content-type", "application/json")
-        self.end_headers()
-        self.wfile.write(bytes(json.dumps(hello), "utf-8"))
-    
-    def do_PATCH(self):
-        self.send_response(200)
-        self.send_header("Content-type", "application/json")
-        self.end_headers()
-        self.wfile.write(bytes(json.dumps(hello), "utf-8"))
 
 if __name__ == "__main__":
     
@@ -55,22 +17,8 @@ if __name__ == "__main__":
     HOSTNAME = CONFIG.get('Server', 'HOSTNAME')
     PORT = CONFIG.get('Server', 'PORT')
 
-    #Start the http server on the local machine. 
-
-    PSServer = HTTPServer((HOSTNAME, int(PORT)), AppServer)
-    '''PSServer.socket = ssl.wrap_socket(
-        PSServer.socket,
-        server_side=True,
-        certfile='/Users/leenton/python/localhost.pem'
-    )'''
     
     
     print("Server started http://%s:%s" % (HOSTNAME, PORT))
 
-    try:
-        PSServer.serve_forever()
-    except KeyboardInterrupt:
-        pass
-    
-    PSServer.server_close()
     print("Server stopped.")
