e8e4e88e245af58fa1a029c341b3b5d6320d898f
leenton <mail@leenton.net>
2023-04-02 13:15:40 +0100

    remove unused code

--- ps1/Start-PSRest.ps1
+++ ps1/Start-PSRest.ps1
@@ -1,3 +1,10 @@
 function Start-PSRest(){
-    #this command will be used to ctually start the server, it will take some arguments with config prefrences maybe? 
+    #this command will be used to actually start the server, it will take some arguments with config prefrences maybe? 
+    #start nats
+
+    #start the objet store
+
+    #Start-PSRest -Port 8080
+    
+
 }
