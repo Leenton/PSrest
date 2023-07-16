3eb286dd9a03d52575668dad51be090e8a52f161
leenton <cakeunit01@gmail.com>
2023-07-16 14:43:44 +0100

    examples

--- test copy.py
+++ test copy.py
@@ -5,31 +5,13 @@ from time import sleep
 
 async def main():
     # Connect to NATS!
-    
 
-    # Simple publisher and async subscriber via coroutine.
-    nc = await nats.connect("localhost:4222")
-    
     while(True):
+        print("Sending")
+        nc = await nats.connect()
         await nc.publish("test", b'Hello')
         print("Sent")
-        sleep(1)
-
-    
-    # msg = await sub.next_msg(60)
-    # now = datetime.now()
-    # count = 0
-    
-    # while(True):
-    #     msg = await sub.next_msg(5)
-    #     print("Received:", msg.data)
-    #     count += 1
-    #     if(count == 1000):
-    #         break
-
-    # print(now)
-    # print(datetime.now())
-        
+        sleep(0.1)
 
 if __name__ == '__main__':
     asyncio.run(main())
