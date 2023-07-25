d8ae0e51ae0cfa3d889a42c31b5ad6614ae3046c
leenton <mail@leenton.net>
2023-07-25 23:13:42 +0100

    Remove comments

--- src/entities/PSRestResponseStream.py
+++ src/entities/PSRestResponseStream.py
@@ -17,34 +17,3 @@ class PSRestResponseStream():
             else:
                 break
         yield None
-
-    # def __init__(self, ticket : PSTicket):
-    #     super().__init__()
-    #     self._is_closed = False
-    #     self.ticket = ticket
-
-    # def __enter__(self):
-    #     return self
-    
-    # def read(self):
-    #     #make a socket connection to the ticket file
-    #     soc = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-    #     soc.bind(f'{RESPONSE_DIR}/{self.ticket}')
-    #     soc.listen(1)
-    #     conn, addr = soc.accept()
-    #     data = conn.read()
-    #     conn.close()
-    #     soc.close()
-
-    #     return data
-
-    # def close(self):
-    #     if not self._is_closed:
-    #         self.response.close()
-    #         self._is_closed = True
-    #     super().close()
-
-    # def __del__(self):
-    #     self.close()
-
-    
